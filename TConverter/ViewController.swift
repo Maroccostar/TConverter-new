744d22be5e96dc393cc4283193c53bf7c7707cfc
Maroccostar <maroccostar@i.ua>
2022-01-29 15:30:13 +0200

    // create connection

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var celciusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider!// create connection
     
     
     @IBAction func sliderChange(_ sender: UISlider) {
