2586be76c61f6d303dee52982d6b0a4edfb55cab
Maroccostar <maroccostar@i.ua>
2022-01-29 20:02:10 +0200

    // TConverter working

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -11,10 +11,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var celciusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!// create connection \+2\+3\+4\+5
-    
+    @IBOutlet weak var slider: UISlider! {// create connection \+2\+3\+4\+5
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+            
+        }
+    }
     
     @IBAction func sliderChange(_ sender: UISlider) {
+        
+        let temperatureCelcius = Int(round(sender.value))
+        celciusLabel.text = "\(temperatureCelcius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
     }
     
 }
